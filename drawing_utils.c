256d8bcd7aed039c3d7599cc812d4cf3a8c273e9
Hydra0xetc <lordxzaodeo@gmail.com>
2025-07-19 05:49:50 +0700

    more updates

--- drawing_utils.c
+++ drawing_utils.c
@@ -1,5 +1,9 @@
-#define _USE_MATH_DEFINES
 #include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "screenshot.h"
 
 // Constants for styling
--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "screenshot.h"
 
 int main(int argc, char *argv[]) {
